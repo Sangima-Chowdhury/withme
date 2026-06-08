155e0c501146f8a0ae07e881ee835766f454cc1d
Sangima Chowdhury <sunjimakarim@gmail.com>
2026-06-08 22:21:08 +0100

    Temp setup-db for location column

--- app.py
+++ app.py
@@ -225,6 +225,15 @@ def logout():
     return redirect(url_for("home"))
 
 
+@app.route("/setup-db")
+def setup_db():
+    with db.engine.connect() as conn:
+        conn.execute(
+            db.text('ALTER TABLE post ADD COLUMN IF NOT EXISTS location VARCHAR(100)'))
+        conn.commit()
+    return "location column added!"
+
+
 # start the Flask application
 if __name__ == "__main__":
     app.run(debug=True)
